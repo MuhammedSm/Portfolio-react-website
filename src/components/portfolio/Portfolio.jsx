279288e7ebfb5fd40d36c6788eccd645d28b477f
MuhammedSm <muhammedseyhmusa5@gmail.com>
2023-06-21 22:27:39 +0300

    adding a project

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -6,12 +6,19 @@ import IMG2 from '../../assets/portfolio2.jpg'
 import IMG3 from '../../assets/portfolio3.jpg'
 import IMG4 from '../../assets/portfolio4.jpg'
 import IMG5 from '../../assets/Smile.jpeg'
+import IMG6 from '../../assets/chatapp.png'
 const data = [
   {
     id:0,
     image:IMG0,
     title:'React-Portfolio-Website',
     github:'https://github.com/MuhammedSm/Portfolio-react-website',
+    
+  },{
+    id:1,
+    image:IMG6,
+    title:'Muhammed Chat(Web Chat App)',
+    github:'https://github.com/MuhammedSm/React-chat-app',
     
   },
   {
@@ -22,26 +29,26 @@ const data = [
     
   },
   {
-    id:1,
+    id:3,
     image:IMG1,
     title:'Personal-Voice-Assistant-Java',
     github:'https://github.com/MuhammedSm/Personal-Voice-Assistant-Java',
     
   },
-  {   id:2, 
+  {   id:4, 
       image:IMG2,
       title: 'Vaccination-Clinic-System-Java', 
       github: 'https://github.com/MuhammedSm/Vaccination-Clinic-System', 
       
   }, 
   { 
-      id: 3, 
+      id: 5, 
       image: IMG3,
       title: 'Movie Rating DataBase Website', 
       github: 'https://github.com/MuhammedSm/movie-rating-db',
        
    },{
-     id: 4, 
+     id: 6, 
       image: IMG4,
       title: 'Frontend-Clothing-Website',
       github: 'https://github.com/MuhammedSm/Clothing-Front-End-Website-main', 
@@ -51,38 +58,36 @@ const data = [
 
 
 
-
-function portfolio() {
-  return (
-    <section id="portfolio">
-      <h5>My Recent Work</h5>
-      <h2>Portfolio</h2>
-      <div className="container portfolio__container">
-       {
-        data.map(({id, image, title, github, demo})=>{
-          return(
-            <article key={id} className="portfolio__item">
-            <div className="portfolio__item-image">
-              <img src={image} alt={title} />
-            </div>
-            <h3>{title}</h3>
-            <div className="portfolio__item-cta">
-            <a href={github} className='btn btn-primary'>Github</a>
-            
-            </div>
-            </article>
-
-
-          )
-        })
-
-       }
-          
-          
-        
-      </div>
-    </section>
-  )
-}
+    function portfolio() {
+      return (
+        <section id="portfolio">
+          <h5>My Recent Work</h5>
+          <h2>Portfolio</h2>
+          <div className="container portfolio__container">
+            {data.map(({ id, image, title, github, demo }) => {
+              return (
+                <article key={id} className="portfolio__item">
+                  <div className="portfolio__item-image">
+                    <img src={image} alt={title} />
+                  </div>
+                  <h3>{title}</h3>
+                  <div className="portfolio__item-cta">
+                    <a href={github} className="btn btn-primary">
+                      Github
+                    </a>
+                    {id === 1 && (
+                      <a href="https://muhammesdseyhmusachatapp.netlify.app" className="btn btn-primary">
+                        Demo
+                      </a>
+                    )}
+                  </div>
+                </article>
+              );
+            })}
+          </div>
+        </section>
+      );
+    }
+    
 
 export default portfolio
